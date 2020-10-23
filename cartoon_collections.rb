e3caa2d09ef256c16ca192a82401e0c8a01c0d33
Riley Iverson <rmiverson99@gmail.com>
2020-10-23 23:03:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   
-  index = 1
+  index = 0
   array.each_with_index do |characters, index|
     puts "#{index}. #{characters}"
     index += 1
